e9b6211d133c8202ae401dcca960cc90d32f5102
md rashidul sumy <rashidulikram406@gmail.com>
2021-03-31 20:51:58 -0400

    change

--- App/Screen/RootStackScreen.js
+++ App/Screen/RootStackScreen.js
@@ -7,7 +7,7 @@ import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 import Dashboard from './Dashboard';
 import AddTaskPage from './AddTaskPage';
-
+import Camera from './Camera';
 
 const RootStack = createStackNavigator();
 
@@ -18,7 +18,7 @@ const RootStackScreen = ({navigation}) => (
         <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
         <RootStack.Screen name="Dashboard" component={Dashboard}/>
         <RootStack.Screen name="AddTaskPage" component={AddTaskPage}/>
-
+        <RootStack.Screen name="Camera" component={Camera}/>
         
         
     </RootStack.Navigator>
